3f0479cf170d469f4e9eb3c136c7852f16c60a47
Katirene <kat.traxler@gmail.com>
2016-02-14 12:58:14 -0600

    initial commit

---
+++ scripts/clientapp.js
@@ -0,0 +1,19 @@
+$(document).ready(funciton() {
+    $(.num-button).on('click', function() {
+
+    });
+
+    $(.func-button).on('click', function() {
+
+    });
+
+    $(.equal-button).on('click', function() {
+
+
+    });
+
+    $(.clear-button).on('click', function() {
+
+
+    });
+});
