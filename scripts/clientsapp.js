ee6f7067ac80b710b7e3b2d0078788dc48b43d02
Katirene <kat.traxler@gmail.com>
2016-02-14 14:35:09 -0600

    on click button sketched out

---
+++ scripts/clientsapp.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    console.log("working");
+        $('.num-button'').on('click', function() {
+            $('.display').attr
+        });
+
+        $('.func-button').on('click', function() {
+            ///locks the first num-buttons clicked as value one.
+            ///the func-button is used for routing in the POST request
+        });
+
+        $('.equal-button').on('click', function() {
+                ////locks the previously entered numbers as value two
+                ///executes function that gets sent as an POST request. routed depending on func button
+
+
+        });
+
+        $('.clear-button').on('click', function() {
+            ////execute function that clears the display.
+
+        });
+});
